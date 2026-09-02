69cf070a72aa8d13028f6d2bb0f4c7c94bc8298a
victorxkd <oliveira.victorfc@gmail.com>
2026-09-01 21:43:22 -0300

    Commit 4: Moving method get_charge from Client to Rental.

--- model.py
+++ model.py
@@ -8,22 +8,26 @@ class Book:
         self.title = title
         self.price_code = price_code
 
-    def title(self) -> str:
-        return self.title
-
-    def price_code(self) -> int:
-        return self.price_code
-
 class Rental:
     def __init__(self, book: Book, days_rented: int):
         self.book = book
         self.days_rented = days_rented
 
-    def book(self) -> Book:
-        return self.book
-
-    def days_rented(self) -> int:
-        return self.days_rented
+    def get_charge(self) -> float:
+        amount = 0
+    
+        # determine amounts for each line
+        if self.book.price_code == Book.REGULAR:
+            amount += 2
+            if self.days_rented > 2:
+                amount += (self.days_rented - 2) * 1.5
+        elif self.book.price_code == Book.NEW_RELEASE:
+            amount += self.days_rented * 3
+        elif self.book.price_code == Book.CHILDREN:
+            amount += 1.5
+            if self.days_rented > 3:
+                amount += (self.days_rented - 3) * 1.5
+        return amount
 
 class Client:
 
@@ -34,22 +38,6 @@ class Client:
     def add_rental(self, rental: Rental):
         self.rentals.append(rental)
 
-    def get_charge(self, rental: Rental) -> float:
-        amount = 0
-
-        # determine amounts for each line
-        if rental.book.price_code == Book.REGULAR:
-            amount += 2
-            if rental.days_rented > 2:
-                amount += (rental.days_rented - 2) * 1.5
-        elif rental.book.price_code == Book.NEW_RELEASE:
-            amount += rental.days_rented * 3
-        elif rental.book.price_code == Book.CHILDREN:
-            amount += 1.5
-            if rental.days_rented > 3:
-                amount += (rental.days_rented - 3) * 1.5
-        return amount
-
     def statement(self) -> str:
 
         total_amount = 0
@@ -57,7 +45,7 @@ class Client:
         result = f"Rental summary for {self.name}\n"
         
         for rental in self.rentals:
-            amount = self.get_charge(rental)
+            amount = rental.get_charge()
 
             # add frequent renter points
             frequent_renter_points += 1
